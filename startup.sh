e454182f8099ebc693b16a2b872c69839281b03b
sola <sola563681790@outlook.com>
2017-11-22 15:00:06 +0800

    ShellTest  src  startup.sh

--- startup.sh
+++ startup.sh
@@ -17,8 +17,8 @@ function check_fail(){
 function build(){
    cd $APP_HOME
    echo "check out and build app files."
-   GIT_DIR=./ShellTest/.git git checkout .
-   GIT_DIR=./ShellTest/.git git pull 
+    ShellTest/.git git checkout .
+    ShellTest/.git git pull 
    ant clean 
    check_fail "ant clean: failed to clean ant build"
    ant
